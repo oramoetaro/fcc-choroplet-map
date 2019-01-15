0c9775099d6e298fef135da4b62ef04e38b2a3d1
oramoetaro <oramoetaro@gmail.com>
2019-01-15 15:04:23 -0600

    US map reached

--- choropleth-map.js
+++ choropleth-map.js
@@ -1,14 +1,30 @@
 (function () {
   const educUrl = "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json";
-  const counUrl = "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json";
+  const countiesJsonUrl = "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json";
+
+  const width = $("#map").width();
+  const height = 600;
+  const path = d3.geoPath();
 
   const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", counUrl, true);
+  xhttp.open("GET", countiesJsonUrl, true);
   xhttp.send();
   xhttp.onload = function () {
+
     const json = JSON.parse(xhttp.responseText);
 
-    $("#map").text(JSON.stringify(json));
+    const svg = d3.select("#map").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    const geojson = topojson.feature(json, json.objects.counties);
+
+    svg.selectAll("path")
+      .data(geojson.features)
+      .enter().append("path")
+      .attr("d", path);
+
+    //$("#map").text(JSON.stringify(json));
   }
 })();
 
